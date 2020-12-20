76b163b8c5c945ac06facb4a27a3e989fde34528
nekolko <70108445+nekolko@users.noreply.github.com>
2020-12-20 17:22:33 +0200

    Update les2_4.py

--- les2_4.py
+++ les2_4.py
@@ -8,14 +8,12 @@ tuple_2 = tuple(randint(1, maxnum) for i in range(Range))
 tuple_3 = tuple(randint(1, maxnum) for i in range(Range))
 print('Tuple 1 : ', tuple_1 , '\n','Tuple 2 : ', tuple_2 , '\n' ,'Tuple 3 : ' , tuple_3 )
 
-
 res1 = set(tuple_1) & set(tuple_2) & set(tuple_3)
 print('Common elements in three tuples :')
 if len(res1) == 0:
     print('There are no common elements.', '\n')
 else:
     print(*res1, sep=', ' )
-
 
 unique1tup = tuple(i for i in tuple_1 if tuple_1.count(i) == 1)
 unique2tup = tuple(i for i in tuple_2 if tuple_2.count(i) == 1)
@@ -33,8 +31,7 @@ if len(unique3tup) > 0:
     print('Tuple 3 :', unique3tup, '\n')
 else:
     print('No unique elemnts', '\n')
-
-
+    
 print('Match in exact place and meaning :', '\n')
 minlen = min(len(tuple_1), len(tuple_2), len(tuple_3))
 counter = 0
